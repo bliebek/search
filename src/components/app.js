98afb866dca48f28768eee40dcb0ada5f7d798b8
Bartosz Liebek <bliebek@gmail.com>
2020-08-17 23:02:03 +0200

    feat(app): connecting form and list

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './app.css';
+import SearchForm from './search-form';
+import SearchList from './search-list';
+import filteredApiData from './../api/api';
 
 export default () => {
-  return (<div className="c-app"></div>);
+  const [ data, setData ] = useState([]);
+  const [ filters, setFilters ] = useState([]);
+  const onFormChange = async fields => {
+    setFilters(fields);
+    setData(await filteredApiData(fields));
+  };
+
+  return (<div className="c-app">
+    <SearchForm onChange={onFormChange} />
+    <SearchList data={data} filters={filters} />
+  </div>);
 }
